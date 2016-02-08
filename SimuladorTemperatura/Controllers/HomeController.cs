fa70f7b8cf0a166d13dafc8833c0d68922825829
David Discua <David.Discua@unitec.edu>
2016-02-07 21:35:33 -0600

    Actualizando Repo

---
+++ SimuladorTemperatura/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimuladorTemperatura.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Simulador()
+        {
+            ViewBag.Message = "Simulador";
+
+            return View();
+        }
+    }
+}
---
+++ SimuladorTemperatura/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimuladorTemperatura.Startup))]
+namespace SimuladorTemperatura
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
